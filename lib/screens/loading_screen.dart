ad88a01fa57f296e9c78e0a3b708d9aefc65acce
Lekhika <rlekhika.regmi@gmail.com>
2025-07-03 13:24:39 +0545

    Starter files

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // user still denied — show a message, or fallback
+        return;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // permissions are permanently denied — open app settings
+      await Geolocator.openAppSettings();
+      return;
+    }
+
+    // now we have permission!
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: locationSettings,
+    );
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
